3a1cc2d696968a2bfd5519b022118b50cb89e830
James Johnson <johnsojr@gmail.com>
2015-07-05 15:15:23 -0500

    refactored graph modes to allow additional modes to be registered.

--- app/scripts/Graph.js
+++ app/scripts/Graph.js
@@ -9,10 +9,17 @@ const $ = require('jquery');
 const Vertex = require('./Vertex');
 const Arc = require('./Arc');
 
-const DRAW_VERTEX_MODE = 'DRAW_VERTEX';
-const DRAW_ARC_MODE = 'DRAW_ARC';
-const MOVE_VERTEX_MODE = 'MOVE_VERTEX';
-const DELETE_MODE = 'DELETE';
+// const DRAW_VERTEX_MODE = 'DRAW_VERTEX';
+// const DRAW_ARC_MODE = 'DRAW_ARC';
+// const MOVE_VERTEX_MODE = 'MOVE_VERTEX';
+// const DELETE_MODE = 'DELETE';
+
+// const ACTION_MODES = [
+//   DRAW_VERTEX_MODE,
+//   DRAW_ARC_MODE,
+//   MOVE_VERTEX_MODE,
+//   DELETE_MODE,
+// ];
 
 class Graph {
 
@@ -28,7 +35,28 @@ class Graph {
       vertices: [],
       arcs: [],
       sketch: null,
-      actionMode: DRAW_VERTEX_MODE,
+      modes: [
+        {
+          id: 'drawVertex',
+          on: [this._onDrawVertexMode],
+          off: [this._offDrawVertexMode],
+        },
+        {
+          id: 'drawArc',
+          on: [this._onDrawArcMode],
+          off: [this._offDrawArcMode],
+        },
+        {
+          id: 'delete',
+          on: [this._onDeleteVertexMode],
+          off: [this._offDeleteVertexMode],
+        }, {
+          id: 'move',
+          on: [this._onMoveVertexMode],
+          off: [this._offMoveVertexMode],
+        },
+      ],
+      currentMode: 'drawVertex',
       displayActionMode: true
     };
 
@@ -38,8 +66,11 @@ class Graph {
       this[key] = value;
     });
 
-    // initialize action mode
-    this.setActionMode(this.actionMode);
+    // cache canvas jquery object for click handlers
+    this.$canvas = $(this.sketch.canvas);
+
+    // initialize current mode
+    this.setCurrentMode(this.currentMode);
 
   }
 
@@ -69,109 +100,135 @@ class Graph {
     return this.vertices.some(vertex => vertex.hasMouseOver());
   }
 
-  getActionMode() {
-    return this.actionMode;
+  getMode(modeName) {
+    return _.find(this.modes, {id: modeName});
   }
 
-  setActionMode(mode) {
+  addMode(modeName, onFn, offFn) {
 
-    this.actionMode = mode;
-    let $canvas = $(this.sketch.canvas);
+    let mode = this.getMode(modeName);
 
-    // remove event handlers from other actions
-    let actionList = [
-      'click.graph.addVertex',
-      'click.graph.drawArc',
-      'click.graph.delete',
-      'mousedown.graph.moveVertex',
-      'mouseup.graph.moveVertex',
+    // if mode exists, add functions
+    if (mode) {
+      this.mode.on.push(onFn);
+      this.mode.off.push(offFn);
+    }
 
-    ];
-    $canvas.off(actionList.join(' '));
+    // otherwise create a new entry in mode list
+    else {
+      this.mode.push({
+        id: modeName,
+        on: [onFn],
+        off: [offFn],
+      });
+    }
+  }
 
-    // Cancel any Arcs that started, but didn't complete
+  getCurrentMode() {
+    return this.getMode(this.currentMode);
+  }
+
+  _onMoveVertexMode() {
+    this.$canvas.on('mousedown.graph.moveVertex',(e) => {
+      e.preventDefault();
+      _.filter(this.vertices, v => v.hasMouseOver())
+       .forEach(v => v.isMoving = true);
+    });
+
+    this.$canvas.on('mouseup.graph.moveVertex', (e) => {
+      e.preventDefault();
+      this.vertices.forEach(v => v.isMoving = false);
+    });
+  }
+
+  _offMoveVertexMode() {
+    this.$canvas.off('mousedown.graph.moveVertex mouseup.graph.moveVertex');
+  }
+
+  _onDrawVertexMode() {
+    this.$canvas.on('click.graph.addVertex',(e) => {
+      e.preventDefault();
+      this.addVertex({
+        x: this.sketch.mouseX,
+        y: this.sketch.mouseY
+      });
+    });
+  }
+  _offDrawVertexMode() {
+    this.$canvas.off('click.graph.addVertex');
+  }
+
+  _onDeleteVertexMode() {
+    this.$canvas.on('click.graph.delete',(e) => {
+      e.preventDefault();
+
+      let vertexToRemove = _.find(this.vertices, v => v.hasMouseOver());
+      this.removeVertex(vertexToRemove);
+    });
+  }
+
+  _offDeleteVertexMode() {
+    this.$canvas.off('click.graph.delete');
+  }
+
+  _onDrawArcMode() {
+    // are we currently drawing an arc?
+    let drawingArc = null;
+    
+    this.$canvas.on('click.graph.drawArc', (e) => {
+      e.preventDefault();
+      let vertexClicked = _.find(this.vertices, v => v.hasMouseOver());
+
+      // we we didn't click on a vertex, we're done
+      if (!vertexClicked) {
+        return;
+      }
+
+      // if we're already drawing an arc, finish it 
+      if (drawingArc) {
+        drawingArc.setHead(vertexClicked);
+        drawingArc = null;
+      }
+
+      // if we haven't started a new arc,
+      // this this vertex create a new arc
+      else {
+        drawingArc = this.addArc({tail: vertexClicked});
+      }
+
+    });
+  }
+
+  _offDrawArcMode() {
+    this.$canvas.off('click.graph.drawArc');
+    // remove any incomplete arcs
     _.remove(this.arcs, arc => arc.head === null );
+  }
 
-    // Moving Mode
-    if ( mode === MOVE_VERTEX_MODE) {
-      $canvas.on('mousedown.graph.moveVertex',(e) => {
-        e.preventDefault();
-        _.filter(this.vertices, v => v.hasMouseOver())
-         .forEach(v => v.isMoving = true);
-      });
+  setCurrentMode(modeID) {
+    this.currentMode = modeID;
 
-      $canvas.on('mouseup.graph.moveVertex', (e) => {
-        e.preventDefault();
-        this.vertices.forEach(v => v.isMoving = false);
-      });
-    }
+    // deregister actions for all modes
+    // TODO: make more performant. Don't need to do
+    // this for all modes, just current and previous
+    let allOffFns = _.flatten(_.pluck(this.modes, 'off'), true);
+    allOffFns.forEach((fn) => {
+      return fn.bind(this)();
+    });
 
-    // Vertex Drawing
-    if (mode === DRAW_VERTEX_MODE) {
-      $canvas.on('click.graph.addVertex',(e) => {
-        e.preventDefault();
-        this.addVertex({
-          x: this.sketch.mouseX,
-          y: this.sketch.mouseY
-        });
-      });
-    }
-
-    // Delete Mode
-    if (mode === DELETE_MODE) {
-      $canvas.on('click.graph.delete',(e) => {
-        e.preventDefault();
-
-        let vertexToRemove = _.find(this.vertices, v => v.hasMouseOver());
-        this.removeVertex(vertexToRemove);
-      });
-    }
-
-    // Arc Drawing
-    if (mode === DRAW_ARC_MODE) {
-      
-      // are we currently drawing an arc?
-      let drawingArc = null;
-      
-      $canvas.on('click.graph.drawArc', (e) => {
-        e.preventDefault();
-
-        let vertexClicked = _.find(this.vertices, v => v.hasMouseOver());
-
-        if (!vertexClicked) {
-          return;
-        }
-
-        // if we're already drawing an arc, finish it 
-        if (drawingArc) {
-          drawingArc.setHead(vertexClicked);
-          drawingArc = null;
-        }
-
-        // if we haven't started a new arc,
-        // this this vertex create a new arc
-        else {
-          drawingArc = this.addArc({tail: vertexClicked});
-        }
-
-      });
-    }
+    // register new mode
+    let currentModeOnFns = this.getCurrentMode().on;
+    currentModeOnFns.forEach((fn) => {
+      return fn.bind(this)();
+    });
   }
 
   cycleActionMode() {
-    let modes = [
-      DRAW_VERTEX_MODE,
-      DRAW_ARC_MODE,
-      MOVE_VERTEX_MODE,
-      DELETE_MODE
-    ];
 
-    let modeIndex = _.findIndex(modes, mode => mode === this.actionMode);
+    let idx = _.findIndex(this.modes, {id: this.currentMode });
+    let nextIdx = (idx + 1) % this.modes.length;
 
-    let nextMode = modes[ (modeIndex + 1) % modes.length];
-
-    this.setActionMode(nextMode);
-
+    this.setCurrentMode(this.modes[nextIdx].id);
   }
 
 
@@ -184,7 +241,7 @@ class Graph {
       s.push();
       s.fill(0);
       s.textSize(32);
-      s.text(this.actionMode, 10, 90);
+      s.text(this.getCurrentMode().id, 10, 90);
       s.pop(); 
     }
 
--- app/scripts/Harmonigraph.js
+++ app/scripts/Harmonigraph.js
@@ -3,14 +3,19 @@
 **/
 
 const _ = require('lodash');
+const $ = require('jquery');
+const p5 = require('p5');
 const Graph = require('./Graph');
+
+require('p5/lib/addons/p5.sound');
 
 class Harmonigraph {
 
   constructor(config) {
     let defaults = {
       graph: null,
-      sketch: null
+      sketch: null,
+      actionMode: 'DRAW_VERTEX',
     };
 
     config = _.assign({}, defaults, config);
@@ -19,10 +24,19 @@ class Harmonigraph {
       this[key] = value;
     });
 
+    // cache $canvas
+    this.$canvas = $(this.sketch.canvas);
+
     // construct a new graph, if there isn't one
     if (!this.graph) {
       this.graph = new Graph({sketch: this.sketch});
     }
+
+    // // add 'PLAY' to graphs action mode list
+    // this.graph.addActionMode('PLAY_MODE');
+
+    // // initialize action mode
+    // this.setActionMode(this.actionMode);
   }
 
   cycleActionMode() {
@@ -30,20 +44,103 @@ class Harmonigraph {
   }
 
   addVertex(config) {
-    this.graph.addVertex(config);
+    let v = this.graph.addVertex(config);
+
+    // add an oscillator to the vertex
+    let osc = new p5.Oscillator();
+    osc.setType('sine');
+    osc.freq(240);
+    osc.amp(0);
+    osc.start();
+    
+    v.osc = osc;
+    v._isPlaying = false;
+    v._defaultColor = v.color;
+
+    return v;
+
   }
 
   addArc(config) {
-    this.graph.addArc(config);
+    return this.graph.addArc(config);
   }
 
   render() {
-    this.graph.render();
+    return this.graph.render();
   }
 
-  setActionMode(mode) {
-    this.graph.setActionMode(mode);
-  }
+  // disableActions() {
+
+  //   // remove event handlers for harmonigraph
+  //   // specific events
+  //   let actionList = [
+  //     'click.harmonigraph.play',
+  //     'click.harmonigraph.addVertex'
+  //   ];
+
+  //   this.$canvas.off(actionList.join(' '));
+  //   this.graph.disableActions();
+  // }
+
+  // _setupPlayMode() {
+  //   console.log('play mode');
+
+  //   this.graph.setActionMode('PLAY_MODE');
+    
+  //   // setup click handler so that when vertex is clicked, oscillator is played
+  //   this.$canvas.on('click.harmonigraph.play', (e) => {
+  //     e.preventDefault();
+
+  //     _.filter(this.graph.vertices, vertex => vertex.hasMouseOver())
+  //       .forEach((vertex) => {
+  //         console.log(vertex);
+
+  //         if (!vertex._isPlaying){
+  //           vertex.color = [0,0,255,100];
+  //           vertex._isPlaying = true;
+  //           vertex.osc.amp(0.25, 0.05);
+  //         } else {
+  //           vertex.color = vertex._defaultColor;
+  //           vertex._isPlaying = false;
+  //           vertex.osc.amp(0, 0.5);
+  //         }
+  //       });
+  //   });
+  // }
+
+  // _setupDrawMode() {
+  //   console.log('Draw Mode');
+
+  //   // setup draw mode, but disable actions
+  //   this.graph.setActionMode('DRAW_VERTEX', false);
+
+  //   // FIXME: this feels ugly. find a better way
+  //   // to override the default addVertex
+  //   // we want to use the Harmonigraph's addVertex
+  //   // on click so that an oscillator is added.
+  //   this.$canvas.on('click.harmonigraph.addVertex', (e) => {
+  //     e.preventDefault();
+  //     this.addVertex({
+  //       x: this.sketch.mouseX,
+  //       y: this.sketch.mouseY
+  //     });
+  //   });
+  // }
+
+  // setActionMode(mode) {
+  //   this.actionMode = mode;
+
+  //   // turn off any existing actions
+  //   this.disableActions();
+
+  //   if (mode === 'PLAY_MODE') {
+  //     this._setupPlayMode();
+  //   } else if (mode === 'DRAW_VERTEX') {
+  //     this._setupDrawMode();
+  //   } else {
+  //     this.graph.setActionMode(mode);
+  //   }
+  // }
 
 }
 
